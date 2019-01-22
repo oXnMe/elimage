eb966b029fff2e556ce9ec5db3b31acad2a1db2e
oXnMe <mellcx@gmail.com>
2019-01-22 11:06:03 +0800

    修改配置文件适用于Docker

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
-DEBUG = True
-DEFAULT_DATA_DIR = '/tmp'
+import os
+
+DEBUG = os.getenv('DEBUG', False)
+DEFAULT_DATA_DIR = os.getenv('ELIMAGE_DATA_DIR','/tmp')
 DEFAULT_PORT = 8888
 DB = 'elimage.db'
 
 XHEADERS = True # you may set this to false if not behind another server
 CLOUDFLARE = False
 
-UPLOAD_PASSWORD = ''
+UPLOAD_PASSWORD = os.getenv('PASSWORD', '')
